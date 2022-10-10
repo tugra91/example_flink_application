d922d9e0d105c256805474b94ca288bb7953fad9
TUGRA <tugraer@gmail.com>
2022-10-10 17:59:34 +0300

    Add Description of Functions.

--- src/main/java/com/flink/application/DataStreamJob.java
+++ src/main/java/com/flink/application/DataStreamJob.java
@@ -70,26 +70,45 @@ public class DataStreamJob {
 				BasicTypeInfo.STRING_TYPE_INFO,
 				BasicTypeInfo.INT_TYPE_INFO};
 		RowCsvInputFormat rowCsvInputFormat = new RowCsvInputFormat(csvFilePath, typeInfoList , "\n", ",");
+
+		/** In this case, we collect data from Csv File as bulk because we have bounded dataset.
+		* Also If you change FileProcessingMode.PROCESS_ONCE to FileProcessingMode.PROCESS_CONTINUOUSLY
+		* You can see your application won't be terminated and after even finish its task, It will wait new data from that source and application will continue to work.
+		* And If you set stream source as FileProcessingMode.PROCESS_CONTINUOUSLY you can't start up application in Batch Mode. */
 		DataStream<Row> dataStreamCsv =  env.readFile(rowCsvInputFormat, csvFilePath.getPath(), FileProcessingMode.PROCESS_ONCE, 10);
 
+		/** We converted Flink Csv Row Object to our POJO type. I called IbbWifi and determined its variables according to Csv Columns.  */
 		DataStream<IbbWifi> modelStreamCsv = dataStreamCsv.map(new RowToModelTransformation());
 
+		/**  We grouped streaming data according to subscriberNationally.  You can change that field as you want.
+		*   For Example If you change like IbbWifi::getDate so then Flink will group data by using date field. Please Try it :)
+		*   If I translate subscriberType Value in csv File. Yerli: Native, Yabancı: Foreigner, Bilinmiyor: Unknown.
+		*/
 		KeyedStream<IbbWifi, String>  keyedStreamCsv = modelStreamCsv.keyBy(IbbWifi::getSubscriberType);
 
+		/**  We calculated total userCount value and assign it to totalUserCount by using data that separate in groups according to subscriberType.
+		*   Also, we used Flink state feature to hold previous totalCount value.
+		*/
 		SingleOutputStreamOperator<IbbWifi> calculationStreamCsv = keyedStreamCsv.map(new CalculationTransformation());
 
+		/**
+		 We converted pojo file to String. It is unnecessary but I added. Because Flink already converted Pojo File to String by using toString method in sink stage.
+		 */
 		SingleOutputStreamOperator<String> modelToTextStreamCsv = calculationStreamCsv.map(new PojoToTextTransformation());
 
-		/* You can change file path according to your file organization. But On my mac,
-			I faced an issue that it didn't save it to location except for the project path.
-			If you are using a Mac, please consider this case.  */
+		/** You can change file path according to your file organization. But On my mac,
+		*  I faced an issue that it didn't save it to location except for the project path.
+		*  If you are using a Mac, please consider this case.  */
 		StreamingFileSink<String> fileSink = StreamingFileSink
 				.forRowFormat(new Path("/Users/tugra/Dev/gerimedica/deneme/ibb_wifi_user_count_csv_outputs"), new SimpleStringEncoder<String>())
 				.build();
 
+		/** Flink write all output to file according to grouped value and parallelism in here. */
 		modelToTextStreamCsv.addSink(fileSink);
 
-
+		/**
+		* All of above processes same for below processes only difference is you use txt file instead of csvFile to fetch data.
+		* */
 		Path filePath = commonUtil.getFileFromResource("ibb_wifi_user_counts.txt");
 		TextInputFormat textInputFormat = new TextInputFormat(filePath);
 		textInputFormat.setFilesFilter(FilePathFilter.createDefaultFilter());
